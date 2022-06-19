6ec7ff1d8db90a3d7392827d132c99fd4185b30a
Timothy Jones <timothy.l.jones@gmail.com>
2022-06-19 23:58:37 +1000

    refactor: Extract TimeDisplay component to its own file

---
+++ src/TimeDisplay.tsx
@@ -0,0 +1,40 @@
+import { DateTime } from 'luxon';
+import { useTimeFormats } from './hooks/useTimeFormats';
+
+export const TimeDisplay = ({
+  timezone,
+  now,
+}: {
+  timezone: string;
+  now: DateTime;
+}) => {
+  const { time, city, displayTimezone, timeOfDay, timeToNine } = useTimeFormats(
+    timezone,
+    now
+  );
+  return (
+    <div className={`TimeBlock ${timeOfDay}`}>
+      <div className="TimeBlockSegment">
+        <div className="City">{city}</div>
+        {timeOfDay === 'lateEvening' || timeOfDay === 'night' ? (
+          'Asleep'
+        ) : (
+          <div className="Time">{time}</div>
+        )}
+      </div>
+
+      <div className="TimeBlockSegment">
+        <div className="Timezone">{displayTimezone}</div>
+        {timeOfDay === 'lateEvening' || timeOfDay === 'night' ? (
+          <div className="Timezone">
+            Awake in <span className="Time">{timeToNine.hours}</span> hr{' '}
+            <span className="Time">{timeToNine.minutes}</span> min and{' '}
+            <span className="Time">{timeToNine.seconds}</span> s{' '}
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </div>
+  );
+};

bcfb6fb5ba529d4a0568b84105e8ad39e546fd23
Timothy Jones <timothy.l.jones@gmail.com>
2022-06-20 23:59:27 +1000

    fix: Add missing key property to cities

--- src/AllClocks/Clock/City.tsx
+++ src/AllClocks/Clock/City.tsx
@@ -1,7 +1,11 @@
 export const City = ({ city }: { city: string | Array<string> }) => (
   <div className="City">
     {Array.isArray(city)
-      ? city.map((name) => <div className="multi-city">{name}</div>)
+      ? city.map((name) => (
+          <div className="multi-city" key={name}>
+            {name}
+          </div>
+        ))
       : city}
   </div>
 );
